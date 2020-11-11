49d77e3be8c372ad3a6050098392d99f2990b210
juiceghost <jg@renegade.se>
2020-11-11 09:49:31 +0100

    Started writing functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,14 +33,43 @@ var remainingGuesses = 0;       // How many tries the player has left
 var hasFinished = false;        // Flag for 'press any key to try again'     
 var wins = 0;                   // How many wins has the player racked up
 
+// 
+let startButton = document.querySelector('div.row>div:nth-child(2)>h2');
+let hangmanImg = document.querySelector('#hangmanImage');
+
 // Game sounds
 var keySound = new Audio('./assets/sounds/typewriter-key.wav');
 var winSound = new Audio('./assets/sounds/you-win.wav');
 var loseSound = new Audio('./assets/sounds/you-lose.wav');
+// document.querySelector('div.row>div:nth-child(2)>h2')
+
+startButton.addEventListener('click', startGame);
+
+// Skriv en funktion som körs när vi klickar på Starta spelet-knappen (jag får icke ändra i .html-filen)
+function startGame(e) {
+    //console.log("GAME STARTED!")
+    //console.log(e.target)
+    // resetta spelets "state" till det urpsrungliga.
+    remainingGuesses = maxTries; // 10
+    hasFinished = false;
+    guessedLetters = [];
+    currentWordIndex = randomWord(selectableWords); // 0..13 (?)
+    console.log(selectableWords[currentWordIndex])
+}
+
+function randomWord(arr) {
+    // givet en array arr
+    // slumpa fram ett (number) index i arrayen. 0 <= i < arr.length
+
+    // vad är min desired range?
+    // let myRandomNumber = Math.floor(Math.random() * arr.length);
+    return Math.floor(Math.random() * arr.length);
+    // should return an index
+}
 
 // Reset our game-level variables
 
-//  Updates the display on the HTML Page
+// Updates the display on the HTML Page
 
 // Updates the image depending on how many guesses
 
