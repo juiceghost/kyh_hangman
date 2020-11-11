73b2e3d7e55ad8bce3e39daaa237eb8cc2381543
juiceghost <jg@renegade.se>
2020-11-11 12:04:55 +0100

    Some more gameplay mechanics

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,8 +35,11 @@ var wins = 0;                   // How many wins has the player racked up
 
 // 
 let startButton = document.querySelector('div.row>div:nth-child(2)>h2');
-let hangmanImg = document.querySelector('#hangmanImage');
-
+let hangmanImgEl = document.querySelector('#hangmanImage');
+let totalWinsEl = document.querySelector('#totalWins');
+let currentWordEl = document.querySelector('#currentWord');
+let remainingGuessesEl = document.querySelector('#remainingGuesses');
+let guessedLettersEl = document.querySelector('#guessedLetters');
 // Game sounds
 var keySound = new Audio('./assets/sounds/typewriter-key.wav');
 var winSound = new Audio('./assets/sounds/you-win.wav');
@@ -45,16 +48,97 @@ var loseSound = new Audio('./assets/sounds/you-lose.wav');
 
 startButton.addEventListener('click', startGame);
 
+document.addEventListener("keypress", guessLetter);
+
+function guessLetter(e) {
+    let currentGuessedLetter = e.key.toUpperCase();
+    let hasUserAlreadyGuessedThisLetter = guessedLetters.includes(currentGuessedLetter)
+    //console.log("Letter exists: " + hasUserAlreadyGuessedThisLetter)
+    if (hasUserAlreadyGuessedThisLetter) {
+        // spelaren har gissat samma bokstav 2 ggr
+        return;
+    } else {
+        // spelaren har gissat bokstaven för första gången
+        guessedLetters.push(currentGuessedLetter)
+    }
+
+    // selectableWords[currentWordIndex]
+    // "TE"
+    // kolla om cGL finns i current word
+    const indexOfFirst = selectableWords[currentWordIndex].indexOf(currentGuessedLetter);
+    if (indexOfFirst > -1) {
+        guessingWord[indexOfFirst] = currentGuessedLetter;
+        // bokstav hittad!
+        //console.log("letter found at: " + indexOfFirst)
+        //console.log(selectableWords[currentWordIndex][indexOfFirst])
+        const indexOfSecond = selectableWords[currentWordIndex].indexOf(currentGuessedLetter, indexOfFirst + 1);
+        if (indexOfSecond > -1) {
+            guessingWord[indexOfSecond] = currentGuessedLetter;
+        }
+        updateCurrentWord();
+        //console.log("second letter found at: " + indexOfSecond)
+        //console.log(selectableWords[currentWordIndex][indexOfSecond])
+
+    } else {
+        // bokstav ej hittad
+
+
+        // guessingWord = ['_', '_', ...] (TE)
+        // använder chansar på T
+        // om ja
+        // in i lettersguessed
+        // ej minska g remaining
+        // bokstaven ska synas i currentword på rätt ställe(n)
+        // (var det den sista bokstaven som saknades?) 
+
+        // om nej gör vi nedan
+        console.log("end of function: " + guessedLetters)
+
+        remainingGuesses = remainingGuesses - 1;
+        updateGuessesRemaining();
+        updateLettersGuessed();
+        updateHangmanImg();
+    }
+}
+// skriv en funktion som skriver ut samma antal "_" som det finns
+// tecken i ordet (currentword, det spelaren ska gissa)
+function updateCurrentWord() {
+    currentWordEl.innerHTML = guessingWord.join("");
+}
+
+function updateHangmanImg() {
+    let imgCount = 10 - remainingGuesses; // 0..10
+    let srcString = "assets/images/" + imgCount + ".png"
+    hangmanImgEl.src = srcString
+}
+
+function updateLettersGuessed() {
+    guessedLettersEl.innerHTML = guessedLetters;
+}
+
+function updateGuessesRemaining() {
+    remainingGuessesEl.innerHTML = remainingGuesses;
+}
+
+// skriv en funktion som håller spelaren informerad om antal gissningar som finns kvar
+
+// skriv en funktion som informerar spelaren om vilka bokstäven som redan gissats
+
+
 // Skriv en funktion som körs när vi klickar på Starta spelet-knappen (jag får icke ändra i .html-filen)
+// Reset our game-level variables
 function startGame(e) {
-    //console.log("GAME STARTED!")
-    //console.log(e.target)
-    // resetta spelets "state" till det urpsrungliga.
     remainingGuesses = maxTries; // 10
     hasFinished = false;
     guessedLetters = [];
     currentWordIndex = randomWord(selectableWords); // 0..13 (?)
+    let currentWordLength = selectableWords[currentWordIndex].length
     console.log(selectableWords[currentWordIndex])
+    guessingWord = "_".repeat(currentWordLength).split("")
+    updateCurrentWord();
+    updateLettersGuessed()
+    updateGuessesRemaining()
+    updateHangmanImg();
 }
 
 function randomWord(arr) {
@@ -67,7 +151,7 @@ function randomWord(arr) {
     // should return an index
 }
 
-// Reset our game-level variables
+
 
 // Updates the display on the HTML Page
 
